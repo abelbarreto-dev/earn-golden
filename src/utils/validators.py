315fe5c45cd43f15ff0e48f4ec3d3be4761346dd
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-26 19:48:20 -0300

    feat: raise exceptions for email and username invalids

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -2,14 +2,19 @@ from re import match
 
 from src.utils.enums import CheckRegex
 
+from src.exceptions.exceptions import (
+    EmailException,
+    UsernameException,
+)
+
 
 class Validator:
     @classmethod
     def check_email(cls, email: str) -> None:
         if not match(CheckRegex.EMAIL.value, email):
-            pass
+            raise EmailException()
 
     @classmethod
     def check_username(cls, username: str):
         if not match(CheckRegex.USERNAME.value, username):
-            pass
+            raise UsernameException()
