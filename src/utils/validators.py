258684128dd3f4aa8ac9bce68f4540341fceba5b
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-26 19:47:22 -0300

    feat: checker username method checker

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -8,3 +8,8 @@ class Validator:
     def check_email(cls, email: str) -> None:
         if not match(CheckRegex.EMAIL.value, email):
             pass
+
+    @classmethod
+    def check_username(cls, username: str):
+        if not match(CheckRegex.USERNAME.value, username):
+            pass
