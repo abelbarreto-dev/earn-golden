519a1ea34ba6473a77e6d603e9b68ac931ea74b3
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-27 18:04:55 -0300

    feat: exceptions for money and percentage

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -6,3 +6,13 @@ class EmailException(ValueError):
 class UsernameException(ValueError):
     def __init__(self):
         super().__init__("username format is not valid")
+
+
+class MoneyException(ValueError):
+    def __init__(self):
+        super().__init__("money format is not valid")
+
+
+class PercentException(ValueError):
+    def __init__(self, name: str = "percentage"):
+        super().__init__(f"{name} format is not valid")
