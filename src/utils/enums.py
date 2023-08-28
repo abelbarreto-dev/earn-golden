196d5482e7730227a85c16c77949fd5d5cfca37b
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-28 10:18:37 -0300

    feat: number card regex

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -31,3 +31,4 @@ class CheckRegex(Enum):
     USERNAME = r"^[a-z_]+[a-z_.0-9]+[a-z0-9_]{1}$"
     MONEY = r"^[0-9]+(?:\.[0-9]{2})?$"
     PERCENT = r"^[0-9]+.[0-9]{1,}$"
+    CARD_NUMBER = r"^[0-9]{16}$"
