90ca9a5ff09f15d015138341720928c5cc833536
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-29 12:18:48 -0300

    feat: class method calculate implemented

--- src/calculate/cnpj_calc.py
+++ src/calculate/cnpj_calc.py
@@ -1,9 +1,42 @@
 from src.exceptions.exceptions import CNPJException
 
+from typing import (
+    List,
+    Generator,
+)
+
 
 class CNPJCalc:
-    def _calculate(self) -> str:
-        return ""
+    def _make_operation_check_digits(
+            self,
+            new_cpf: Generator[int, None, None],
+            digits: List[int]
+    ) -> int:
+        pass
+
+    def _calculate(self, cnpj: str) -> str:
+        check_digits = ""
+
+        new_cnpj = [int(i) for i in cnpj]
+
+        check_digit = self._make_operation_check_digits(
+            (i for i in new_cnpj),
+            [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+        )
+
+        check_digits += str(check_digit)
+        new_cnpj.append(check_digit)
+
+        check_digit = self._make_operation_check_digits(
+            (i for i in new_cnpj),
+            [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2],
+        )
+
+        new_cnpj.clear()
+
+        check_digits += str(check_digit)
+
+        return check_digits
 
     def cnpj_calc_checker(self, cnpj: str) -> None:
         if not cnpj.isnumeric():
