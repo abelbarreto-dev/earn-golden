a79df96f9b1d7c177541c24e3ce71e44fd9f86d5
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-29 01:29:57 -0300

    chore: create class cpf calc to validate cpf

--- src/calculate/cpf_calc.py
+++ src/calculate/cpf_calc.py
@@ -3,67 +3,68 @@ from typing import Generator
 from src.exceptions.exceptions import CPFException
 
 
-def _get_multiply_calc_list(
-        new_cpf: Generator[int, None, None],
-        digit: int = 10,
-) -> Generator[int, None, None]:
-    for index, multi in zip(new_cpf, range(digit, 1, -1)):
-        yield index * multi
+class CPFCalc:
+    @classmethod
+    def _get_check_digit(cls, sum_all: int) -> int:
+        module_eleven = sum_all % 11
 
+        check_digit = (11 - module_eleven, 0)[module_eleven < 2]
 
-def _get_check_digit(sum_all: int) -> int:
-    module_eleven = sum_all % 11
+        return check_digit
 
-    check_digit = (11 - module_eleven, 0)[module_eleven < 2]
+    @classmethod
+    def _get_multiply_calc_list(
+            cls,
+            new_cpf: Generator[int, None, None],
+            digit: int = 10,
+    ) -> Generator[int, None, None]:
+        for index, multi in zip(new_cpf, range(digit, 1, -1)):
+            yield index * multi
 
-    return check_digit
+    def _make_operation_check_digits(
+            self,
+            new_cpf: Generator[int, None, None],
+            digits: int = 10,
+    ) -> int:
+        list_sum_all = self._get_multiply_calc_list(new_cpf, digits)
 
+        sum_all = sum(i for i in list_sum_all)
 
-def _make_operation_check_digits(
-        new_cpf: Generator[int, None, None],
-        digits: int = 10,
-) -> int:
-    list_sum_all = _get_multiply_calc_list(new_cpf, digits)
+        check_digit = self._get_check_digit(sum_all)
 
-    sum_all = sum(i for i in list_sum_all)
+        return check_digit
 
-    check_digit = _get_check_digit(sum_all)
+    def _calculate(self, cpf: str) -> str:
+        check_digits = ""
 
-    return check_digit
+        new_cpf = [int(i) for i in cpf[: -2]]
 
+        check_digit = self._make_operation_check_digits(
+            (i for i in new_cpf),
+        )
 
-def _calculate(cpf: str) -> str:
-    check_digits = ""
+        check_digits += str(check_digit)
+        new_cpf.append(check_digit)
 
-    new_cpf = [int(i) for i in cpf[: -2]]
+        check_digit = self._make_operation_check_digits(
+            (i for i in new_cpf),
+            digits=11
+        )
 
-    check_digit = _make_operation_check_digits(
-        (i for i in new_cpf),
-    )
+        new_cpf.clear()
 
-    check_digits += str(check_digit)
-    new_cpf.append(check_digit)
+        check_digits += str(check_digit)
 
-    check_digit = _make_operation_check_digits(
-        (i for i in new_cpf),
-        digits=11
-    )
+        return check_digits
 
-    new_cpf.clear()
+    def cpf_calc_checker(self, cpf: str) -> None:
+        if not cpf.isnumeric():
+            raise CPFException("cpf value should be numeric only")
 
-    check_digits += str(check_digit)
+        if len(cpf) not in (11, ):
+            raise CPFException("cpf length should be equals to 11")
 
-    return check_digits
+        check_digits = self._calculate(cpf)
 
-
-def cpf_calc_checker(cpf: str) -> None:
-    if not cpf.isnumeric():
-        raise CPFException("cpf value should be numeric only")
-
-    if len(cpf) not in (11, ):
-        raise CPFException("cpf length should be equals to 11")
-
-    check_digits = _calculate(cpf)
-
-    if check_digits not in cpf[-2: 11]:
-        raise CPFException("cpf checker digits are not valid")
+        if check_digits not in cpf[-2: 11]:
+            raise CPFException("cpf checker digits are not valid")
