63fcc79fbf1b9744c0c0a0c21dc58efdeaa77fa0
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-08-31 15:47:45 -0300

    hotfix: app build module

--- app.py
+++ app.py
@@ -3,16 +3,5 @@ from fastapi import FastAPI
 from src.routes.routes import route
 
 
-def get_fast_api() -> FastAPI:
-    return FastAPI()
-
-
-def get_app_set_route() -> FastAPI:
-    app = get_app_set_route()
-
-    app.include_router(route)
-
-    return app
-
-
-app = get_app_set_route()
+app = FastAPI()
+app.include_router(route)
