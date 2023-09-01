9a2c516d7cef9a282936ddb320af1dde85048421
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-09-01 09:04:14 -0300

    feat: create new fixture session to access database

--- test/conftest.py
+++ test/conftest.py
@@ -6,8 +6,18 @@ from typing import Generator
 
 from app import app
 
+from src.database.connection import get_session
+
+from sqlalchemy.orm import SessionTransaction
+
 
 @pytest.fixture(scope="function")
 def client() -> Generator[TestClient, None, None]:
     with TestClient(app) as earn_golden:
         yield earn_golden
+
+
+@pytest.fixture(scope="function")
+def session() -> Generator[SessionTransaction, None, None]:
+    with get_session().begin() as db_session:
+        yield db_session
