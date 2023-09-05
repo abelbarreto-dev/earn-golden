b51722f2e11adb5623fefa88407128df1a1fd787
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-09-05 13:05:52 -0300

    feat: search class name by regex

--- src/utils/regex_serch.py
+++ src/utils/regex_serch.py
@@ -1,1 +1,16 @@
+from re import search
 
+
+def get_class_name(
+        original: str,
+) -> str:
+    regex = r"<class '.*?\.([A-Za-z]+)'>"
+
+    new_search = search(regex, original)
+
+    if new_search is None:
+        raise Exception("the class name wasn't found")
+
+    new_word = new_search.group(1)
+
+    return new_word
