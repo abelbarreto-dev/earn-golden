6efe95e522aeefbd8c5a2800ad03c9dfad472401
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-09-17 11:58:39 -0300

    feat: repository deposit linked to its service

--- src/services/service_deposit.py
+++ src/services/service_deposit.py
@@ -1,2 +1,12 @@
+from src.repositories.repository_deposit import RepositoryDeposit
+
+
 class ServiceDeposit:
-    pass
+    _repo_deposit: RepositoryDeposit
+
+    def __init__(self):
+        self._repo_deposit = RepositoryDeposit()
+
+    @property
+    def repo_deposit(self) -> RepositoryDeposit:
+        return self._repo_deposit
