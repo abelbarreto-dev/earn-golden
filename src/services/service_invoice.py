f469c59f4fdb456eb0b0b9d4f6149023fab1bae0
Abel Carvalho <64908368+abelbarreto-dev@users.noreply.github.com>
2023-09-17 12:03:32 -0300

    feat: repositories card deposit and invoice to their services (#81)
    * feat: repository card linked to its service
    
    * feat: repository deposit linked to its service
    
    * feat: repository invoice linked to its service

--- src/services/service_invoice.py
+++ src/services/service_invoice.py
@@ -1,2 +1,12 @@
+from src.repositories.repository_invoice import RepositoryInvoice
+
+
 class ServiceInvoice:
-    pass
+    _repo_invoice: RepositoryInvoice
+
+    def __init__(self):
+        self._repo_invoice = RepositoryInvoice()
+
+    @property
+    def repo_invoice(self) -> RepositoryInvoice:
+        return self._repo_invoice
